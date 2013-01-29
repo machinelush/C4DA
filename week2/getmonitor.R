a5acaa61777e4a01607a37f16a7739afa36c14eb
Shaun <shaun@moorchel.net>
2013-01-28 21:37:48 -0500

    Add notes, vids and assignments for all 4 weeks

--- week2/getmonitor.R
+++ week2/getmonitor.R
@@ -1,14 +1,21 @@
 getmonitor <- function(id, directory, summarize = FALSE) {
-        ## 'id' is a vector of length 1 indicating the monitor ID
-        ## number. The user can specify 'id' as either an integer, a
-        ## character, or a numeric.
-        
-        ## 'directory' is a character vector of length 1 indicating
-        ## the location of the CSV files
+## 'id' is a vector of length 1 indicating the monitor ID
+## number. The user can specify 'id' as either an integer, a
+## character, or a numeric.
 
-        ## 'summarize' is a logical indicating whether a summary of
-        ## the data should be printed to the console; the default is
-        ## FALSE
-        
-        ## Your code here
+## 'directory' is a character vector of length 1 indicating
+## the location of the CSV files
+
+## 'summarize' is a logical indicating whether a summary of
+## the data should be printed to the console; the default is
+## FALSE
+
+id <- toString(id)        
+if (length(id) == 1) { id <- paste(0,0,id, sep='')
+} else if (length(id) == 2 ) { id <- paste(0,id, sep='') } 
+else { id <- paste(id, sep='')}
+
+data <- read.csv(file.path(getwd(),directory,paste(id,".csv",sep='')),header=T)
+if (summarize == T) { print(summary(data))}
+
 }
